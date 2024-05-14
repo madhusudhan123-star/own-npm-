9ba933a08125a8c05be1146a7c1898d472c9879d
Madhusudhan <dmadhusudhan98@gmail.com>
2024-05-14 09:25:16 +0530

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+function basiceRequest(){
+    const http = require('http');
+    const fs = require('fs');
+    const path = require('path');
+    const PORT = 4000;
+    console.log("hi");
+} 
+
+
+module.exports = basiceRequest;
