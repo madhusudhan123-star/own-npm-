741b15d2508df8d24abff6197c2f9c88dae6359a
Madhusudhan <dmadhusudhan98@gmail.com>
2024-05-18 16:35:06 +0530

    add

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 const path = require('path');
 const PORT = 4000;
 const url = require('url');
-console.log("hi");
+const express = require('express');
+
 
 module.exports = {
     http,
     fs,
     path,
     url,
-    PORT
+    PORT,
+    express
 };
